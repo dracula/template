516733c33ed4da28f38708eaede7ca6bb0acca95
Lucas de França <luxon4uta@gmail.com>
2023-11-20 20:43:22 -0300

    ⚙️ Add initial project files and configurations

--- sample/dracula.js
+++ sample/dracula.js
@@ -3,25 +3,25 @@
  */
 class Vampire {
   constructor(props) {
-    this.location = props.location
-    this.birthDate = props.birthDate
-    this.deathDate = props.deathDate
-    this.weaknesses = props.weaknesses
+    this.location = props.location;
+    this.birthDate = props.birthDate;
+    this.deathDate = props.deathDate;
+    this.weaknesses = props.weaknesses;
   }
 
   get age() {
-    return this.calcAge()
+    return this.calcAge();
   }
 
   calcAge() {
-    return this.deathDate - this.birthDate
+    return this.deathDate - this.birthDate;
   }
 }
 
 // ...there was a guy named Vlad
 const Dracula = new Vampire({
-  location: 'Transylvania',
+  location: "Transylvania",
   birthDate: 1428,
   deathDate: 1476,
-  weaknesses: ['Sunlight', 'Garlic'],
-})
+  weaknesses: ["Sunlight", "Garlic"],
+});
--- sample/dracula.ts
+++ sample/dracula.ts
@@ -3,39 +3,39 @@
  */
 
 interface VampireProps {
-  location: string
-  birthDate: number
-  deathDate: number
-  weaknesses: string[]
+  location: string;
+  birthDate: number;
+  deathDate: number;
+  weaknesses: string[];
 }
 
 class Vampire {
-  location: string
-  birthDate: number
-  deathDate: number
-  weaknesses: string[]
+  location: string;
+  birthDate: number;
+  deathDate: number;
+  weaknesses: string[];
 
   constructor(props: VampireProps) {
-    this.location = props.location
-    this.birthDate = props.birthDate
-    this.deathDate = props.deathDate
-    this.weaknesses = props.weaknesses
+    this.location = props.location;
+    this.birthDate = props.birthDate;
+    this.deathDate = props.deathDate;
+    this.weaknesses = props.weaknesses;
   }
 
   get age(): number {
-    return this.calcAge()
+    return this.calcAge();
   }
 
   calcAge(): number {
-    return this.deathDate - this.birthDate
+    return this.deathDate - this.birthDate;
   }
 }
 
 // ...there was a guy named Vlad
 
 const Dracula: VampireProps = new Vampire({
-  location: 'Transylvania',
+  location: "Transylvania",
   birthDate: 1428,
   deathDate: 1476,
-  weaknesses: ['Sunlight', 'Garlic'],
-})
+  weaknesses: ["Sunlight", "Garlic"],
+});
